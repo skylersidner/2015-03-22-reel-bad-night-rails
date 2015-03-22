8269c996e689f60134242c9c58af3f05fac3e553
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-22 14:34:07 -0500

    added attr_accessible for fields, validation, and DB relationship copy/pasted from sinatra version;

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -1,3 +1,10 @@
 class Patron < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :username, :password
   
+  has_and_belongs_to_many :events
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
 end
