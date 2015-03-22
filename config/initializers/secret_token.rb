5c6e7e941606d6ff45d6a520fa6f3ce587df464e
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-22 13:54:18 -0500

    first commit;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReelBadNightRails::Application.config.secret_token = '69428a80e551b966bcd9ef3d2f0a6c20c019da8e7f9da548b94343ea96738dee99e7abf621eb6c7295121af551dc2c0ed52ca7892b502565d96b6e06b5899a69'
