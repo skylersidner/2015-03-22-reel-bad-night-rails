7058e20927e40d0aca405206b0670677b10bf02a
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-23 11:24:12 -0500

    drinks fully adapted and functioning;

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -9,23 +9,35 @@ class DrinksController < ApplicationController
   end
   
   def create
-
+    @object = Drink.new(params[:drink])
+    if @object.save
+      redirect_to "/drinks/#{@object.id}"
+    else      
+      render "new"
+    end
   end
   
   def show
-
+    @object = Drink.find_by_id(params[:id])
   end
   
   def edit
-
+    @object = Drink.find_by_id(params[:id])
   end
   
   def update
-
+    @object = Drink.find_by_id(params[:id])
+    if @object.update_attributes(params[:drink])
+      redirect_to "/drinks/#{@object.id}"
+    else      
+      render "edit"
+    end
   end
   
   def delete
-
+    @object = Drink.find_by_id(params[:id])
+    @object.destroy
+    redirect_to "/drinks"
   end
 
 
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,31 +2,43 @@ class EventsController < ApplicationController
 
 
   def index
-
+    @results = Event.all
   end
   
   def new
-
+    @object = Event.new
   end
   
   def create
-
+    @object = Event.new(params[:event])
+    if @object.save
+      redirect_to "/events/#{@object.id}"
+    else      
+      render "new"
+    end
   end
   
   def show
-
+    @object = Event.find_by_id(params[:id])
   end
   
   def edit
-
+    @object = Event.find_by_id(params[:id])
   end
   
   def update
-
+    @object = Event.find_by_id(params[:id])
+    if @object.update_attributes(params[:event])
+      redirect_to "/events/#{@event.id}"
+    else      
+      render "edit"
+    end
   end
   
   def delete
-
+    @object = Event.find_by_id(params[:id])
+    @object.destroy
+    redirect_to "/events"
   end
 
 
