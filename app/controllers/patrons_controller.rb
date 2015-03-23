00e1e5e73356e6c62d10b54bb86273b907557f75
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-23 12:08:38 -0500

    patrons fully functional;

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -2,31 +2,43 @@ class PatronsController < ApplicationController
 
 
   def index
-
+    @results = Patron.all
   end
   
   def new
-
+    @object = Patron.new
   end
   
   def create
-
+    @object = Patron.new(params[:patron])
+    if @object.save
+      redirect_to "/patrons/#{@object.id}"
+    else      
+      render "new"
+    end
   end
   
   def show
-
+    @object = Patron.find_by_id(params[:id])
   end
   
   def edit
-
+    @object = Patron.find_by_id(params[:id])
   end
   
   def update
-
+    @object = Patron.find_by_id(params[:id])
+    if @object.update_attributes(params[:patron])
+      redirect_to "/patrons/#{@object.id}"
+    else      
+      render "edit"
+    end
   end
   
   def delete
-
+    @object = Patron.find_by_id(params[:id])
+    @object.destroy
+    redirect_to "/patrons"
   end
 
 
