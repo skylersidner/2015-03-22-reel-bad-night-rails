7a212b381746fa8eb5701cb88a989b4e156c1bf9
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-23 19:45:36 -0500

    minor cleanup;

--- config/routes.rb
+++ config/routes.rb
@@ -65,22 +65,4 @@ ReelBadNightRails::Application.routes.draw do
   delete "/patrons/:id" => 'patrons#delete', :as => "patron"
   
 
-  #FOR REFERENCE FROM MINI RAILS PROJECT
-  # root to: 'dinners#homepage'
-  #
-  # # Index
-  # get "/dinners" => 'dinners#index', :as => "dinners"
-  # # New
-  # get "/dinners/new" => 'dinners#new', :as => "new_dinner"
-  # # Create
-  # post "/dinners" => 'dinners#create', :as => "dinners"
-  # # Show
-  # get "/dinners/:id" => 'dinners#show', :as => "dinner"
-  # # Edit
-  # get "/dinners/:id/edit" => 'dinners#edit', :as => "edit_dinner"
-  # # Update
-  # put "/dinners/:id" => 'dinners#update', :as => "dinner"
-  # # # Destroy
-  # delete "/dinners/:id" => 'dinners#delete', :as => "dinner"
-
 end
