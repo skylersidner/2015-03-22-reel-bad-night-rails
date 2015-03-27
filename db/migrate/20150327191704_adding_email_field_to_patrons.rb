28b61b34c27228db301b968ac597c60a8db5312d
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-27 14:25:28 -0500

    fixed migration change method;

--- db/migrate/20150327191704_adding_email_field_to_patrons.rb
+++ db/migrate/20150327191704_adding_email_field_to_patrons.rb
@@ -1,7 +1,5 @@
 class AddingEmailFieldToPatrons < ActiveRecord::Migration
   def change
-    change_table :patrons do |t|
-      add_column :email, :string
-    end
+    add_column :patrons, :email, :string
   end
 end
