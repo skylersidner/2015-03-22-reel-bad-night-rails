e5ff23a55b73baff8e92dc60d1b6d15291924037
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-27 10:54:49 -0500

    homepage now displays top 2 events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
 
   def homepage
-    @current_event = Event.find_by_current_event(2)
+    @current_event = Event.order('start_time DESC')[0]
     if @current_event != nil
       @current_film = Film.find_by_id(@current_event.film_id)
     end
-    @previous_event = Event.find_by_current_event(1)
+    @previous_event = Event.order('start_time DESC')[1]
     if @previous_event != nil
       @previous_film = Film.find_by_id(@previous_event.film_id)
-    end
+    end    
   end
   
 end
