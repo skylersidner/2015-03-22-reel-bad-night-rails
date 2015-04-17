ebe09528dce9c08e28d7f02b73f68f974dde8e04
Skyler Sidner <malevolentdragon@gmail.com>
2015-04-17 16:13:52 -0500

    added comments to films_helper.rb for code sample;

--- app/helpers/films_helper.rb
+++ app/helpers/films_helper.rb
@@ -10,19 +10,26 @@ module FilmsHelper
   #
   # State Changes: @results is populated with objects.
   def get_rt_search_results
+    #use rotten-tomatoes gem to search for films based on title
     search_results = RottenMovie.find(:title => "#{params[:search]}", :limit => 20)
-    if search_results.length == nil #check if search returned a single object
+
+    #rotten-tomatoes gem is unpredictable; if the search gets nothing it returns an empty array; if it gets many things, they are returned in an array; but if it gets only one thing, it returns the RottenTomatoes object itself; this condition ensures that the method will always return an array;
+    if search_results.length == nil #check if search returns a non-array object
       search_results = [search_results] #make it an array
     end
-    search_results.each do |x| #capture relevant info from rottentomatoes
+    
+    #convert the information into an array of film objects
+    search_results.each do |x|
       film = Film.new("title"=>"#{x.title}", "year"=>"#{x.year}", "length"=>"#{x.runtime}", "synopsis"=>"#{x.synopsis}", "trailer"=>"", "rt_rating"=>"#{x.ratings.critics_score}")
       @results << film
       
+      #create an array of poster thumbnails; if a poster is unavailable, use a default image
       if x.posters.thumbnail == nil
         @thumbs << "../images/poster_default.gif"
       else
         @thumbs << x.posters.thumbnail
       end #if
+      
     end #each
     @results
   end #method
